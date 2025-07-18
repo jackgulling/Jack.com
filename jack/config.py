9cd2749d634ea06b75a12147da9c78c187cc4a4a
jackgulling <jdgulling1@gmail.com>
2025-07-18 13:21:30 -0600

    Chapter 15 Restructure

--- jack/config.py
+++ jack/config.py
@@ -1,19 +1,22 @@
 import os
+from dotenv import load_dotenv
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
-	SECRET_KEY = os.environ.get('SECRET_KEY') or 'password1'
-	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-		'sqlite:///' + os.path.join(basedir, 'app.db')
-	MAIL_SERVER = 'localhost'
-	MAIL_PORT = 25
-	MAIL_USE_TLS = False
-	MAIL_USE_SSL = False 
-	MAIL_USERNAME = None
-	MAIL_PASSWORD = None
-	MAIL_DEFAULT_SENDER = 'jack@jack.com'
-	ADMINS = ['jack@jack.com']
-	POSTS_PER_PAGE = 25
-	LANGUAGES = ['en-US', 'es']
-	MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['your-email@example.com']
+    LANGUAGES = ['en', 'es']
+    MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
+    POSTS_PER_PAGE = 25
+
+
 	
